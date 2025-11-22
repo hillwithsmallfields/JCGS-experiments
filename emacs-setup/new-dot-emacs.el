4f16994b9819c73d80dc91b9ba7da1bce237b39f
John Sturdy <jcg.sturdy@gmail.com>
2025-11-22 14:34:00 +0000

    remove the bits that weren't needed / didn't work, and start configuring it

--- emacs-setup/new-dot-emacs.el
+++ emacs-setup/new-dot-emacs.el
@@ -1,21 +1,8 @@
 ;; experiments towards a new .emacs
 
-(use-package straight
-  ;; :custom (straight-use-package-by-default t)
-  )
-
-;; (straight-use-package 'use-package)
+(use-package straight)
 
 (straight-use-package-mode 1)
-
-;; (use-package emms
-;;   :straight t)
-
-;; (use-package '(emms :fetcher github :repo "emacsmirror/emms")
-;;   :straight t)
-
-;; (use-package emms
-;;   :straight '(emms :fetcher github :repo "emacsmirror/emms"))
 
 (use-package emms
   :straight '(emms :fetcher github :repo "git@github.com:emacsmirror/emms.git")

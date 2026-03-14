74ac578119ba2395b3a113d5200d03011f125336
John Sturdy <jcg.sturdy@gmail.com>
2026-03-14 21:09:05 +0000

    First attempt at org website export

---
+++ org-publish-test/publish-test.el
@@ -0,0 +1,23 @@
+(require 'ox-publish)
+;; initially based on https://orgmode.org/worg/org-tutorials/org-publish-html-tutorial.html
+
+(cl-pushnew '("publishing-test"
+              :components ("notes" "static"))
+            org-publish-project-alist)
+(cl-pushnew '("notes"
+              :base-directory "~/open-projects/github.com/hillwithsmallfields/JCGS-experiments/org-publish-test/"
+              :base-extension "org"
+              :publishing-directory "~/scratch/test-published/"
+              :recursive t
+              :publishing-function org-html-publish-to-html
+              :headline-levels 4
+              :auto-preamble t)
+            org-publish-project-alist)
+(cl-pushnew '("static"
+              :base-directory "~/open-projects/github.com/hillwithsmallfields/JCGS-experiments/org-publish-test/"
+              :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
+              :publishing-directory "~/scratch/test-published/"
+              :recursive t
+              :publishing-function org-publish-attachment)
+            org-publish-project-alist)
+      
